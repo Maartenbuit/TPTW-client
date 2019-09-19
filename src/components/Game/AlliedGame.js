2c39c38a1b95b12d94985aadd61d1b847ffe0b57
pattyouwehand <pattyouwehand@gmail.com>
2019-09-19 16:07:56 +0200

    axis-game room id

--- src/components/Game/AlliedGame.js
+++ src/components/Game/AlliedGame.js
@@ -1,13 +1,22 @@
 import React from 'react'
-//import { Link } from 'react-router-dom'
+// import { request } from 'https';
+// import { url } from '../../constants'
+// import { connect } from 'react-redux'
 
-export default class AlliesGame extends React.Component {
-
-  render(){
-    return(
+export default class AlliedGame extends React.Component {
+  render() {
+    console.log('this.props.alliedQuestions', this.props.alliedQuestions)
+    const answers = this.props.alliedQuestions[0].answers.map(answer =>
+      <button value={answer} onClick={this.props.handleEvent}>{answer}</button>
+    )
+    return (
       <div>
         <h1 className= 'allies'>You are the allied force</h1>
         <p>The major Allied powers in World War I were Great Britain (and the British Empire), France, and the Russian Empire, formally linked by the Treaty of London of September 5, 1914. Other countries that had been, or came to be, allied by treaty to one or more of those powers were also called Allies: Portugal and Japan by treaty with Britain; Italy by the Treaty of London of April 26, 1915, with all three powers. Other countries—including the United States after its entry on April 6, 1917—that were arrayed against the Central Powers were called “Associated Powers,” not Allied powers; U.S. President Woodrow Wilson emphasized that distinction to preserve America’s free hand. The Treaty of Versailles (June 28, 1919) concluding the war listed 27 “Allied and Associated Powers”: Belgium, Bolivia, Brazil, the British Empire, China, Cuba, Czechoslovakia, Ecuador, France, Greece, Guatemala, Haiti, the Hejaz, Honduras, Italy, Japan, Liberia, Nicaragua, Panama, Peru, Poland, Portugal, Romania, Serb-Croat-Slovene State, Siam, the United States, and Uruguay.</p>
+        
+        <h1>Question: </h1>
+        <p>{this.props.question}</p>
+        {answers}
       </div>
     )
   }
--- src/components/Game/AlliedGameContainer.js
+++ src/components/Game/AlliedGameContainer.js
@@ -1,15 +1,74 @@
 import React, { Component } from 'react'
-import AlliesGame from '../Game/AlliesGame'
+import AlliedGame from '../Game/AlliedGame'
+import request from 'superagent'
+import { url } from '../../constants'
+import { connect } from 'react-redux'
 
+class AlliedGameContainer extends Component {
+  alliedQuestions = [
+    { q1: 'Allies: Who started the first WW?', answers: ['Germany', 'Holland', 'Poland'] },
+    { q2: 'Allies: Who started the 2nd WW?', answers: ['1', '2', '3'] },
+    { q3: 'Allies: Who started the 3rd WW?', answers: ['a', 'b', 'c'] }
+  ]
 
+  handleEvent = (event) => {
+    if (event.target.value === this.alliedQuestions[0].answers[0]) {
+      // console.log(axisQuestions[0].answers[0])
 
+      request
+        .put(`${url}/user/${this.props.user.userId}/alliedgame`)
+        .send({
+          score: +1,
+          answered: true
+        })
+        .catch(console.error)
 
-export default class AlliedGameContainer extends Component {
+      return alert('true')
+
+    } else {
+      request
+        .put(`${url}/user/${this.props.user.userId}/alliedgame1`)
+        .send({
+          answered: true
+        })
+        .catch(console.error)
+      return alert('FALSE!!!!')
+    }
+  }
+
   render() {
+    const room = this.props.rooms.filter(room => {
+      //console.log("ROOM.USER",room.users)
+      if(this.props.user.userId === room.users[0].id){
+        return room
+      }else if(this.props.user.userId === room.users[1].id){
+        return room
+      }
+    })
+    const questionNumber = room[0].round
+    console.log(questionNumber)
+    const question = this.alliedQuestions[0].q1
+    //const answers1 = this.axisQuestions[0].answers
+    console.log('alliedQuestions[0].answers', this.alliedQuestions[0].answers)
+    console.log('this.alliedQuestions test:', this.alliedQuestions)
     return (
-       <AlliesGame />
+      <AlliedGame
+        handleEvent={this.handleEvent}
+        alliedQuestions={this.alliedQuestions} 
+        question={question} />
     )
   }
 }
+
+const mapStateToProps = (state) => {
+  console.log(' LOG state', state)
+
+  return {
+    user: state.user,
+    rooms: state.rooms 
+  }
+}
+
+export default connect(mapStateToProps)(AlliedGameContainer)
 
 
