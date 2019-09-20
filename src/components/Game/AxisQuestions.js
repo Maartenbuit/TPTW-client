4e2cfdb04857576ad8603265caffb441aa02d263
gijsmaas82 <gijsmaas82@outlook.com>
2019-09-20 15:31:45 +0200

    questions finished

--- src/components/Game/AxisQuestions.js
+++ src/components/Game/AxisQuestions.js
@@ -1,7 +1,11 @@
 const axisQuestions = [
-  { q: 'Who started the first WW?', answers: ['Germany', 'Holland', 'Poland'] },
-  { q: 'Who started the 2nd WW?', answers: ['a', 'b', 'c'] },
-  { q: 'Who started the 3rd WW?', answers: ['a', 'b', 'c'] }
+  { q: 'When did the First World War start?', answers: ['1914', '1912', '1915'] },
+  { q: 'Which country invaded Serbia after the assassination of Archduke Franz Ferdinand?', answers: ['Austria-Hungary', 'Germany', 'Ottoman Empire'] },
+  { q: 'How many soldiers did the Axis Powers mobilize during the war approximately?', answers: ['25 million', '10 million', '5 million'] },
+  { q: 'Who was the emperor of Germany during World War 1?', answers: ['Kaiser Wilhelm II', 'Emperor Franz Josef', 'Adolf Hitler'] },
+  { q: 'Which of the following countries was not part of the Axis Forces?', answers: ['France', 'Bulgaria', 'Ottoman Empire'] },
+  { q: 'What was a major driving force behind the willingness of the European countries to go to war?', answers: ['nationalism', 'colonialism', 'capitalism'] },
+  { q: 'What were the Axis Forces actually called during the First World War?', answers: ['Central Powers', 'the Axis of Evil', 'the nazis'] }
 ]
 
 export default axisQuestions
--- src/components/Game/alliedQuestions.js
+++ src/components/Game/alliedQuestions.js
@@ -1,7 +1,11 @@
 const alliedQuestions = [
-  { q: 'Allies: Who started the first WW?', answers: ['ZGermany', 'Holland', 'Poland'] },
-  { q: 'Allies: Who started the 2nd WW?', answers: ['a', 'b', 'c'] },
-  { q: 'Allies: Who started the 3rd WW?', answers: ['a', 'b', 'c'] }
+  { q: 'When did the First World War start?', answers: ['1914', '1912', '1915'] },
+  { q: 'Which country was not part of the Allied Powers during the First World War?', answers: ['Bulgaria', 'Japan', 'Russia'] },
+  { q: 'Who was the czar of Russia at the start of World War 1?', answers: ['Nicolas II', 'Lenin', 'Rasputin'] },
+  { q: 'From which country did Holland get about a million refugees?', answers: ['Belgium', 'Germany', 'France'] },
+  { q: 'In what year was the battle of Verdun which caused about 300.000 casualties?', answers: ['1916', '1915', '1917'] },
+  { q: 'In what year did America join the Allied Forces?', answers: ['1917', '1916', 'never'] },
+  { q: 'Which gas was used for the first time to kill everyone on the battlefields?', answers: ['Mustardgas', 'Zyklon-b', 'Axe'] }
 ]
 
 export default alliedQuestions
